fa785ca1ba1bdea4d2c5ad60b0a3fc962af2bf76
Pierre Wieser <pwieser@trychlos.org>
2023-02-15 19:05:37 +0100

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:options',
-    version: '1.0.0',
+    version: '1.0.1-rc',
     summary: 'Configuration options management',
     git: 'https://github.com/trychlos/pwix-options',
     documentation: 'README.md'
