01d34c844142a094edf849d33ce9f1090d8f511d
Pierre Wieser <pwieser@trychlos.org>
2023-06-21 18:54:09 +0200

    Rename as global.js the code which defines the global object

--- src/common/js/global.js
+++ src/common/js/global.js
@@ -1,10 +1,13 @@
 /*
- * pwix:options/src/common/js/options.js
+ * pwix:options/src/common/js/global.js
  */
 
 import { Options } from '../classes/options.class.js';
 
 pwixOptions = {
+    // package configuration
+    _conf: {},
+
     // the exported class
     Options: Options
 };
--- src/common/js/index.js
+++ src/common/js/index.js
@@ -2,7 +2,7 @@
  * pwix:options/src/common/js/index.js
  */
 
-import './options.js';
+import './global.js';
 //
 import './constants.js';
 //
